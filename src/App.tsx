62af25b4373783c847686ba81cf346b1b35be48e
Michał Burzyński <michal.burzynski@olx.pl>
2022-10-11 12:37:24 +0200

    bla bla

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,164 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, MouseEventHandler, useCallback } from "react";
+import { ThemeProvider } from "@remirror/react";
+import { AllStyledComponent } from "@remirror/styles/emotion";
+import { CountExtension } from "@remirror/extension-count";
+import {
+  BoldExtension,
+  ItalicExtension,
+  PlaceholderExtension,
+  BulletListExtension,
+} from "remirror/extensions";
 
-function App() {
+import {
+  Remirror,
+  useRemirror,
+  useActive,
+  useCommands,
+  useRemirrorContext,
+  useEditorEvent,
+  useHelpers,
+  ToggleBoldButton,
+  ToggleItalicButton,
+  ToggleBulletListButton,
+} from "@remirror/react";
+import styled from "@emotion/styled";
+
+const StyledEditorComponent = styled.div`
+  background-color: #f2f4f5;
+  border-radius: 4px;
+  padding: 14px 16px;
+
+  & ul {
+    margin: 0;
+  }
+
+  & .ProseMirror {
+    padding: 0 !important;
+    min-height: 40px !important;
+  }
+`;
+
+const EditorWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const CounterWrapper = styled.span`
+  align-self: flex-end;
+`;
+
+const Counter = () => {
+  const { getCharacterCount } = useHelpers(true);
+  const count = getCharacterCount();
+
+  return <CounterWrapper>{count}/520</CounterWrapper>;
+};
+
+const MyEditorComponent = () => {
+  const { getRootProps } = useRemirrorContext();
+  const { getHTML } = useHelpers();
+  const rootProps = getRootProps();
+
+  useEditorEvent("blur", () => {
+    console.log(getHTML());
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <EditorWrapper>
+      <StyledEditorComponent {...rootProps}>
+        <Menu />
+      </StyledEditorComponent>
+      <Counter />
+    </EditorWrapper>
+  );
+};
+
+export const Menu = () => {
+  const [visible, setVisible] = useState(false);
+  const { toggleBold, focus, toggleItalic, toggleBulletList } = useCommands();
+  const active = useActive();
+
+  useEditorEvent("blur", () => setVisible(false));
+  useEditorEvent("focus", () => setVisible(true));
+
+  const handleMouseDown: MouseEventHandler<HTMLButtonElement> = useCallback(
+    (e) => {
+      e.preventDefault();
+    },
+    []
+  );
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div>
+      <ToggleBoldButton />
+      <ToggleItalicButton />
+      <ToggleBulletListButton />
+      <button
+        onMouseDown={handleMouseDown}
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          toggleBold();
+          focus();
+        }}
+        style={{ fontWeight: active.bold() ? "bold" : undefined }}
+      >
+        B
+      </button>
     </div>
   );
-}
+};
+export const MyEditor = () => {
+  const { manager, state } = useRemirror({
+    extensions: () => [
+      // these extensions are needed to create bullet list only editor - may be used in the future
+      // new DocExtension({ content: "bulletList+" }),
+      // new ListItemExtension({
+      //   nodeOverride: { content: "text*" },
+      // }),
+      new BulletListExtension({}),
+      new BoldExtension(),
+      new ItalicExtension(),
+      new PlaceholderExtension({
+        placeholder:
+          "Write down the main responsibilities (e.g. unpacking deliveries, preparing salads)",
+      }),
+      new CountExtension({ maximum: 520 }),
+    ],
+    content: "",
+    selection: "start",
+    stringHandler: "html",
+  });
+
+  return (
+    <AllStyledComponent>
+      <ThemeProvider
+        theme={{
+          color: {
+            border: "none",
+            active: { border: "none" },
+            outline: "none",
+          },
+        }}
+      >
+        <Remirror manager={manager} initialContent={state}>
+          <MyEditorComponent />
+        </Remirror>
+      </ThemeProvider>
+    </AllStyledComponent>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: 16 }}>
+      <MyEditor />
+    </div>
+  );
+};
 
 export default App;
